fdf36f12cc0cbc4d050780af60cda5759e1713a8
ABrooksGit <Abrooks6211996@gmail.com>
2025-04-24 09:40:54 -0400

    Reading from the Books.txt file

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,10 +1,12 @@
 package com.pluralsight;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class Main {
-
-    private static Scanner scanner = new Scanner(System.in);
     private static Book[] library = getPopulatedBooks();
     private static Console console = new Console();
 
@@ -15,32 +17,45 @@ public class Main {
 
 
     private static Book[] getPopulatedBooks() {
-        Book[] library = new Book[20];
 
-        library[0] = new Book(1, "978-1-78862-355-1", "Nginx HTTP Server");
-        library[1] = new Book(2, "978-0-321-35668-0", "Clean Code");
-        library[2] = new Book(3, "978-1-4919-1889-0", "Fluent Python");
-        library[3] = new Book(4, "978-1-4919-1880-7", "Python Cookbook");
-        library[4] = new Book(5, "978-0-596-52068-7", "Head First Design Patterns");
-        library[5] = new Book(6, "978-0-13-468599-1", "Effective Java");
-        library[6] = new Book(7, "978-0-13-235088-4", "Clean Architecture");
-        library[7] = new Book(8, "978-1-4919-1881-4", "Learning Python");
-        library[8] = new Book(9, "978-1-59327-599-0", "Automate the Boring Stuff with Python");
-        library[9] = new Book(10, "978-0-262-03384-8", "Introduction to Algorithms");
-        library[10] = new Book(11, "978-1-4919-5609-0", "Designing Data-Intensive Applications");
-        library[11] = new Book(12, "978-1-59327-928-8", "Eloquent JavaScript");
-        library[12] = new Book(13, "978-1-4493-6132-7", "You Don’t Know JS Yet");
-        library[13] = new Book(14, "978-1-4842-0070-4", "Pro Git");
-        library[14] = new Book(15, "978-1-59327-144-2", "Hacking: The Art of Exploitation");
-        library[15] = new Book(16, "978-0-13-110362-7", "The C Programming Language");
-        library[16] = new Book(17, "978-1-59327-733-8", "The Linux Command Line");
-        library[17] = new Book(18, "978-1-4919-1881-4", "Deep Learning with Python");
-        library[18] = new Book(19, "978-1-4493-9306-9", "High Performance Browser Networking");
-        library[19] = new Book(20, "978-1-4919-5246-7", "Kubernetes Up & Running");
+        try {
+            FileReader fr = new FileReader("books.txt");
+            BufferedReader reader = new BufferedReader(fr);
+
+            Book[] booksTemp = new Book[1000];
+            int size = 0;
+            String dataString;
+
+            while ((dataString = reader.readLine()) != null) {
+
+                booksTemp[size] = getBookFromEncodedString(dataString);
+
+                size++;
+            }
+            Book[] bookFinal = Arrays.copyOf(booksTemp, size);
+
+            return bookFinal;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
 
-        return library;
     }
+
+
+    private static Book getBookFromEncodedString(String encodedBook) {
+        String[] temp = encodedBook.split(Pattern.quote("|"));
+
+        int id = Integer.parseInt(temp[0].trim());
+        String isbn = temp[1];
+        String title = temp[2];
+
+        Book result = new Book(id, isbn, title);
+        return result;
+
+    }
+
+
 
     private static void showAvailableBooks(){
         int i;
@@ -145,8 +160,8 @@ public class Main {
 
 
         do{
-            System.out.print(checkOutScreenPrompt);
-            input = scanner.nextLine();
+
+            input = console.promptForString(checkOutScreenPrompt);
 
             if(input.equals("1")) {
 
@@ -207,3 +222,25 @@ public class Main {
 }
 
 
+//        Book[] library = new Book[20];
+//
+//        library[0] = new Book(1, "978-1-78862-355-1", "Nginx HTTP Server");
+//        library[1] = new Book(2, "978-0-321-35668-0", "Clean Code");
+//        library[2] = new Book(3, "978-1-4919-1889-0", "Fluent Python");
+//        library[3] = new Book(4, "978-1-4919-1880-7", "Python Cookbook");
+//        library[4] = new Book(5, "978-0-596-52068-7", "Head First Design Patterns");
+//        library[5] = new Book(6, "978-0-13-468599-1", "Effective Java");
+//        library[6] = new Book(7, "978-0-13-235088-4", "Clean Architecture");
+//        library[7] = new Book(8, "978-1-4919-1881-4", "Learning Python");
+//        library[8] = new Book(9, "978-1-59327-599-0", "Automate the Boring Stuff with Python");
+//        library[9] = new Book(10, "978-0-262-03384-8", "Introduction to Algorithms");
+//        library[10] = new Book(11, "978-1-4919-5609-0", "Designing Data-Intensive Applications");
+//        library[11] = new Book(12, "978-1-59327-928-8", "Eloquent JavaScript");
+//        library[12] = new Book(13, "978-1-4493-6132-7", "You Don’t Know JS Yet");
+//        library[13] = new Book(14, "978-1-4842-0070-4", "Pro Git");
+//        library[14] = new Book(15, "978-1-59327-144-2", "Hacking: The Art of Exploitation");
+//        library[15] = new Book(16, "978-0-13-110362-7", "The C Programming Language");
+//        library[16] = new Book(17, "978-1-59327-733-8", "The Linux Command Line");
+//        library[17] = new Book(18, "978-1-4919-1881-4", "Deep Learning with Python");
+//        library[18] = new Book(19, "978-1-4493-9306-9", "High Performance Browser Networking");
+//        library[19] = new Book(20, "978-1-4919-5246-7", "Kubernetes Up & Running");
